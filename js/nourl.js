d5db0fb44a689aa864e268afcda311fd26a46327
Artem Yankov <artem@badgeville.com>
2012-06-12 22:11:43 -0700

    initial skeleton

---
+++ js/nourl.js
@@ -0,0 +1,93 @@
+var nourl = {
+  run: function(settings, callback) {
+    
+    nourl.settings = settings;
+
+    // vertx.connect(settings.host, settings.port, function() {
+    //   nourl.requireList(settings.require, callback)
+    // });
+  },
+
+  // function that creates stubs for methods of the required class
+  require: function(name, callback) {  
+    nourl.rpc_exec(name + '.rpc_white_list', [], function(stub){
+
+      stub = stub || {result:[]};
+
+      obj = {};
+
+      //create stubs for methods
+      for(i in stub.result) {
+        method = stub.result[i];
+
+        obj[method] = (function(method){
+         return function() {      
+            params = Array.prototype.slice.call(arguments);
+
+            func = arguments[arguments.length - 1];
+          
+            nourl.rpc_exec(name + "." + method, params, function(message) {
+              func(message);  
+            })
+           }
+        })(method);
+      }
+
+      callback(obj);
+    });
+  },
+
+  // create stubs for an array of classes
+  requireList: function(list, callback) {  
+    nourl.rpc_exec('nourl.rpc_white_list_for', [list], function(stub){
+      stub = stub || {result:[]}
+
+      //create stubs for methods
+      for(i in stub.result) {
+        methods = stub.result[i];
+        for (x in methods) {
+          (function(class_name, method) {
+            window[i] = window[i] || {}
+            window[i][method] = function() {      
+                params = Array.prototype.slice.call(arguments);
+
+                if (typeof arguments[arguments.length - 1] === "function") {
+                  func = arguments[arguments.length - 1]  
+                } else {
+                  func = null;
+                }
+
+                nourl.rpc_exec(class_name + "." + method, params, function(message) {
+                  if (func) func(message);
+                });
+            }
+          
+          })(i, methods[x])
+        }
+      }
+
+      callback();
+    });
+  },
+
+  // sends json-rpc formatted string to an eventbus through sockjs
+  rpc_exec: function(method_name, params, callback) {
+    vertx.connection.send(
+       vertx.eventbus_channel, 
+       nourl.json_rpc_format(method_name, params, vertx.rpc_id()), function(message) {
+
+         if (message.error) throw method_name + ": " + message.error;
+
+         // nourl.Events.trigger(method_name + ":complete", window, message); 
+        
+         callback(message);
+       }
+     );
+  },
+
+  // formats given arguments in json-rpc format
+  json_rpc_format: function(method_name, params, id) {
+    return {"method": method_name, "params": params, "id": id};
+  }
+}
+
